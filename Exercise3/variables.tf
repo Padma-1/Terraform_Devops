3b93a373349febb9df873bbbfd9f4af05835518d
Padma-1 <65107839+Padma-1@users.noreply.github.com>
2025-11-26 23:12:59 +0530

    Update variables.tf

--- Exercise3/variables.tf
+++ Exercise3/variables.tf
@@ -1,1 +1,14 @@
+variable "region" {
+  default = "us-east-1"
+}
+variable "zone1" {
+  default = "us-east-1a"
+}
 
+variable "amiID" {
+  type = map(any)
+  default = {
+    us-east-2 = "ami-036841078a4b68e14"
+    us-east-1 = "ami-0ecb62995f68bb549"
+  }
+}
